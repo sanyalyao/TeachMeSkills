549ab1653cca48e6b4ae21f0accf29d2c95edd39
sanyalyao <xxxx81971@yandex.ru>
2023-04-09 14:09:44 +0500

    Added home_1 and home_2

---
+++ home_1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace home_1
+{
+    class Program
+    {
+        /// <summary>
+        /// Написать программу для вывода «Hello <user>» на консоль. Пусть <user> вводиться с консоли, программа должна потребовать ввод<user>. Для чтения с консоли используйте Console.ReadLine().
+        /// </summary>
+        static void Main()
+        {
+            Console.Write("Введите имя пользователя: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello, {userName}");
+        }
+    }
+}
