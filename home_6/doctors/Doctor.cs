8375faba8e547b2e228bdee445c218901d1b9355
sanyalyao <xxxx81971@yandex.ru>
2023-04-18 00:45:44 +0500

    home_6

---
+++ home_6/doctors/Doctor.cs
@@ -0,0 +1,17 @@
+﻿namespace home_6.doctors
+{
+    class Doctor
+    {
+        public string Name { get; set; }
+
+        public Doctor(string name)
+        {
+            this.Name = name;
+        }
+
+        public virtual void Treat(Patient patient)
+        {
+
+        }
+    }
+}
---
+++ home_6/doctors/Ophthalmologist.cs
@@ -0,0 +1,15 @@
+﻿namespace home_6.doctors
+{
+    class Ophthalmologist : Doctor
+    {
+        public Ophthalmologist(string name) : base(name)
+        {
+        }
+
+        public override void Treat(Patient patient)
+        {
+            Console.WriteLine($"{this.Name} will take care of {patient.Name}");
+            Console.WriteLine();
+        }
+    }
+}
