c845c9637509a38c770c68fc0c75ea0993d3973b
sanyalyao <xxxx81971@yandex.ru>
2023-04-26 20:26:51 +0500

    fixes

---
+++ home_9/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace home_9
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random random = new Random();
+
+            List<Person> people = new List<Person>()
+            {
+                new Person("Alex", random.Next(17, 55), random.Next(50, 1500)),
+                new Person("Igor", random.Next(17, 55), random.Next(50, 1500)),
+                new Person("Pasha", random.Next(17, 55), random.Next(50, 1500)),
+                new Person("Pasha", random.Next(17, 55), random.Next(50, 1500)),
+                new Person("Pasha", random.Next(17, 55), random.Next(50, 1500)),
+                new Person("Pasha", random.Next(17, 55), random.Next(50, 1500)),
+                new Person("Pasha", random.Next(17, 55), random.Next(50, 1500)),
+                new Person("Pasha", random.Next(17, 55), random.Next(50, 1500)),
+                new Person("Pasha", random.Next(17, 55), random.Next(50, 1500)),
+            };
+
+            InfoAboutPerson check = new InfoAboutPerson();
+            check.GetNameStartsA(people);
+            check.ShowSalaryAndAge(people);
+            check.ShowPersonHighestAge(people);
+        }
+    }
+}
