cbdf6e1bcb8c9bff6db347ebf6426767aeae64eb
sanyalyao <xxxx81971@yandex.ru>
2023-05-22 19:13:32 +0500

    home_11

---
+++ home_11/Tests/BaseTest.cs
@@ -0,0 +1,21 @@
+﻿namespace home_11.Tests
+{
+    class BaseTest
+    {
+        protected int x;
+        protected int y;
+
+        [SetUp]
+        public void Setup()
+        {
+            x = 6;
+            y = 3;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
