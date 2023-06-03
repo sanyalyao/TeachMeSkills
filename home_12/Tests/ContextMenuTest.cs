7c812e78bcab9c00113a9ae898810b1db46281fa
sanyalyao <xxxx81971@yandex.ru>
2023-06-03 15:21:45 +0500

    home_14

---
+++ home_12/Tests/ContextMenuTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using home_12.Helpers;
+using OpenQA.Selenium.Interactions;
+
+namespace home_12.Tests
+{
+    class ContextMenuTest : TestBase
+    {
+        [Test]
+        [Description("Context Menu " +
+            "i. Правый клик по элементу " +
+            "ii. Валидация текста на алерте " +
+            "iii. Закрытие алерта")]
+        public void CheckContextMenu()
+        {
+            By hotSpot = By.Id("hot-spot");
+
+            driver.FindElement(By.LinkText("Context Menu")).Click();
+
+            WaitHelper.WaitElement(driver, hotSpot);
+
+            Actions action = new Actions(driver);
+            IWebElement square = driver.FindElement(hotSpot);
+
+            action.ContextClick(square).Perform();
+
+            IAlert alert = driver.SwitchTo().Alert();
+            string alertText = alert.Text;
+
+            Assert.AreEqual("You selected a context menu", alertText);
+
+            alert.Accept();
+        }
+    }
+}
---
+++ home_12/Tests/DynamicControlsTest.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using home_12.Helpers;
+
+namespace home_12.Tests
+{
+    class DynamicControlsTest : TestBase
+    {
+        [Test]
+        [Description("Dynamic Controls " +
+            "a. Найти чекбокс " +
+            "b. Нажать на кнопку " +
+            "c. Дождаться надписи “It’s gone” " +
+            "d. Проверить, что чекбокса нет " +
+            "e. Найти инпут " +
+            "f. Проверить, что он disabled " +
+            "g. Нажать на кнопку " +
+            "h. Дождаться надписи “It's enabled!” " +
+            "i. Проверить, что инпут enabled")]
+        public void CheckDynamicControls()
+        {
+            By checkboxBy = By.Id("checkbox");
+            By messageCheckbox = By.CssSelector("form[id='checkbox-example'] > p[id='message']");
+            By messageInputField = By.CssSelector("form[id='input-example'] > p[id='message']");
+
+            driver.FindElement(By.LinkText("Dynamic Controls")).Click();
+
+            WaitHelper.WaitElement(driver, checkboxBy);
+
+            driver.FindElement(By.CssSelector("button[onclick='swapCheckbox()']")).Click();
+
+            WaitHelper.WaitElement(driver, messageCheckbox);
+
+            string finalMessageCheckbox = driver.FindElement(messageCheckbox).Text;
+
+            Assert.AreEqual("It's gone!", finalMessageCheckbox);
+            Assert.IsEmpty(driver.FindElements(checkboxBy));
+
+            IWebElement inputField = driver.FindElement(By.CssSelector("input[type='text']"));
+
+            Assert.IsTrue(!inputField.Enabled);
+
+            driver.FindElement(By.CssSelector("button[onclick='swapInput()']")).Click();
+
+            WaitHelper.WaitElement(driver, messageInputField);
+
+            string finalMessageInputField = driver.FindElement(messageInputField).Text;
+
+            Assert.AreEqual("It's enabled!", finalMessageInputField);
+            Assert.IsTrue(inputField.Enabled);
+        }
+    }
+}
