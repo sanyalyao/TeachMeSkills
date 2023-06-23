c7a0edb0cb2cbeebd706c721070a9c0d756096c3
sanyalyao <xxxx81971@yandex.ru>
2023-06-23 22:52:51 +0500

    home_19 and home_20

---
+++ home_19/BusinessObject/Services/ProjectService.cs
@@ -0,0 +1,67 @@
+﻿using home_19.BusinessObject.Models;
+using home_19.Core;
+using RestSharp;
+
+namespace home_19.BusinessObject.Services
+{
+    public class ProjectService : BaseService
+    {
+        public string GetProjectByCodeEndpoint = "/project/{code}";
+        public string AccessToProjectByCodeEndpoint = "/project/{code}/access";
+        public string ProjectEndpoint = "/project";
+
+        public ProjectService(BaseAPIClient apiClient) : base(apiClient) { }
+
+        public RestResponse GetProjectByCode(string projectCode)
+        {
+            var request = new RestRequest(GetProjectByCodeEndpoint).AddUrlSegment("code", projectCode);
+
+            return apiClient.Execute(request);
+        }
+
+        public RestResponse GetProjects()
+        {
+            var request = new RestRequest(ProjectEndpoint);
+
+            return apiClient.Execute(request);
+        }
+
+        public RestResponse DeleteProjectByCode(string projectCode)
+        {
+            var request = new RestRequest(GetProjectByCodeEndpoint, Method.Delete).AddUrlSegment("code", projectCode);
+
+            return apiClient.Execute(request);
+        }
+
+        public RestResponse GrantAccessToProjectByCode(string projectCode, MemberModel member)
+        {
+            var request = new RestRequest(AccessToProjectByCodeEndpoint, Method.Post).AddUrlSegment("code", projectCode);
+            request.AddBody(member);
+
+            return apiClient.Execute(request);
+        }
+
+        public RestResponse RevokeAccessToProjectByCode(string projectCode, MemberModel member)
+        {
+            var request = new RestRequest(AccessToProjectByCodeEndpoint, Method.Post).AddUrlSegment("code", projectCode);
+            request.AddBody(member);
+
+            return apiClient.Execute(request);
+        }
+
+        public RestResponse CreateProject(CreateProjectModel project)
+        {
+            var request = new RestRequest(ProjectEndpoint, Method.Post);
+            request.AddBody(project);
+
+            return apiClient.Execute(request);
+        }
+
+        public Project GetProjectByCode<ProjectType>(string projectCode) where ProjectType : Project
+        {
+            var request = new RestRequest(GetProjectByCodeEndpoint).AddUrlSegment("code", projectCode);
+
+            return apiClient.Execute<CommonResultResponse<Project>>(request).Result;
+        }
+    }
+}
---
+++ home_19/Core/BaseAPIClient.cs
@@ -0,0 +1,40 @@
+﻿using RestSharp;
+
+namespace home_19.Core
+{
+    public class BaseAPIClient
+    {
+        private RestClient restClient;
+
+        public BaseAPIClient(string url)
+        {
+            var option = new RestClientOptions(url)
+            {
+                MaxTimeout = 10000,
+                ThrowOnAnyError = true,
+            };
+
+            restClient = new RestClient(url);
+            restClient.AddDefaultHeader("Content-Type", "application/json");
+        }
+
+        public void AddToken(string token)
+        {
+            restClient.AddDefaultHeader("token", token);
+        }
+
+        public RestResponse Execute(RestRequest request)
+        {
+            var response = restClient.Execute(request);
+
+            return response; 
+        }
+
+        public T Execute<T>(RestRequest request)
+        {
+            var response = restClient.Execute<T>(request);
+
+            return response.Data;
+        }
+    }
+}
